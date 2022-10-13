1329e9078330bfe639dbc520a9446da9065da39b
gabriel-melo <gabrielmvalley@gmail.com>
2022-10-13 00:24:46 -0300

    Criando componente header e post

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
+
       <Post 
         author="Diego Fernandes" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas omnis alias eaque modi harum nostrum, at similique inventore incidunt quia cumque beatae, odit sed quibusdam. Totam eum laudantium eveniet sed!" 
+      />
+      <Post 
+        author="Gabriel Melo" 
+        content="Um novo post muito legal" 
       />
     </div>
   )
