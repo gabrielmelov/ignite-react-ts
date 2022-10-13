694ffc60e93e883b30150ce47fb3cf3f63fdeed2
gabriel-melo <gabrielmvalley@gmail.com>
2022-10-13 19:07:41 -0300

    Wrapper da divisão entre Sidebar e Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from './App.module.css'
+
 import './global.css'
 
 export function App() {
@@ -7,15 +11,19 @@ export function App() {
     <div>
       <Header />
 
-
-      <Post 
-        author="Diego Fernandes" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas omnis alias eaque modi harum nostrum, at similique inventore incidunt quia cumque beatae, odit sed quibusdam. Totam eum laudantium eveniet sed!" 
-      />
-      <Post 
-        author="Gabriel Melo" 
-        content="Um novo post muito legal" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="Diego Fernandes" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas omnis alias eaque modi harum nostrum, at similique inventore incidunt quia cumque beatae, odit sed quibusdam. Totam eum laudantium eveniet sed!" 
+          />
+          <Post 
+            author="Gabriel Melo" 
+            content="Um novo post muito legal" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
