fb3104bbb14b69eedb3328b23bf52ea94ce1271d
gabriel-melo <gabrielmvalley@gmail.com>
2022-10-15 17:01:32 -0300

    Criando form para comentario e adicionando Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from './avatar'
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/gabrielmelov.png" />
+                    <Avatar src="https://github.com/gabrielmelov.png" />
                     <div className={styles.authorInfo}>
                         <strong>Gabriel Melo</strong>
                         <span>Web Developer</span>
@@ -18,12 +20,30 @@ export function Post() {
             <div className={styles.content}>
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉 <a href=""> jane.design/doctorcare</a></p>
+                <p><a href="">jane.design/doctorcare</a></p>
                 <p>
-                    <a href="">#novoprojeto</a>
-                    <a href="">#nlw</a>
+                    <a href="">#novoprojeto</a>{" "}
+                    <a href="">#nlw</a>{" "}
                     <a href="">#rocketseat</a>
                 </p>   
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea 
+                    placeholder="Deixe um comentário"  
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commmentList}>
+                <Comment />
+                <Comment />
+                <Comment />
             </div>
         </article>
     )
