9f57122874e92f0b23a30ed2d41cc9090143cb6c
gabriel-melo <gabrielmvalley@gmail.com>
2022-10-17 18:21:12 -0300

    Alterando foto de perfil do usuário

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ export function Sidebar() {
             />
             
             <div className={styles.profile}>
-                <Avatar src="https://github.com/maykbrito.png" />
+                <Avatar src="https://github.com/gabrielmelov.png" />
 
                 <strong>Gabriel Melo</strong>
                 <span>Web Developer</span>
